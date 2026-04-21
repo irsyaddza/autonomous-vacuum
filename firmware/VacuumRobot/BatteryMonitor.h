3e159f37d390da9d6055fe3550430f920d367c4a
Irsyad Zainul Abidin <irsyadza04@gmail.com>
2026-04-21 22:30:57 +0700

    change: battery 2s to 3s

--- firmware/VacuumRobot/BatteryMonitor.h
+++ firmware/VacuumRobot/BatteryMonitor.h
@@ -13,8 +13,9 @@ private:
     // Calibration factor for Voltage Divider
     // Vout = Vin * (R2 / (R1 + R2))
     // ADC reads Vout. Vin = ADC * (3.3 / 4095) * ((R1+R2)/R2)
-    // Using R1=10kΩ, R2=4.7kΩ: (10k + 4.7k) / 4.7k = 3.128
-    const float _calibrationFactor = 3.128;
+    // Using R1=30kΩ, R2=10kΩ: (30k + 10k) / 10k = 4.0
+    // 3S max 12.6V → Vout = 12.6 * 10/40 = 3.15V (safe for ESP32 ADC)
+    const float _calibrationFactor = 4.0;
 };
 
 #endif
